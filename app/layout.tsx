3ca36981b20de12d2159fb09700ce5c2ae523e17
ulrich2k25 <ulrichpouke150@gmail.com>
2025-06-23 00:10:55 +0200

    Fix: clean unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from 'next/link';
-import { usePathname, useSearchParams } from 'next/navigation';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
